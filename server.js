aa72d5cce18e12326e1c700f5e98a1823d12ed20
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2023-06-14 16:33:26 +0900

    setting

--- server.js
+++ server.js
@@ -2,8 +2,17 @@
 const express =require("express")
 const app = express();
 const PORT = 50000;
-// const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+app.use(express.json());
+
 
 app.listen(PORT, () => {
   console.log("サーバーが起動中・・・");
 });
+
+app.get("/items", async (req, res) => {
+  const items = await prisma.items.findMany();
+  return res.json(items);
+});
+
