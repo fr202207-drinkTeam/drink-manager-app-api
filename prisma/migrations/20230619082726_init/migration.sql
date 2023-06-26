8f2ba00bb2ef2d8b4aba01f0ed5a0407602d532d
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2023-06-26 09:42:58 +0900

    エラー修正

--- prisma/migrations/20230619082726_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "Posts" ADD CONSTRAINT "Posts_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Items"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20230619085353_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "ItemImages" ADD CONSTRAINT "ItemImages_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Items"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
